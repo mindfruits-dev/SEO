bfd989f2ca59f9f41d310fdd24a5be73b2802f70
archist <webdev.archist@gmail.com>
2020-01-20 11:13:13 +0100

    'amended'

---
+++ LifeCycles.js
@@ -0,0 +1,13 @@
+function doGet(e) {
+  var html = HtmlService.createTemplateFromFile('index');
+
+  html.myData = "thisismydata;)"
+
+  return html.evaluate()
+}
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
+function getLaunched(tmp){
+  return "okok";
+}
